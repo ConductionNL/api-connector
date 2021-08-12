b8695245927e7dd5cca7236844d4aa734d701ecc
Ruben van der Linde <ruben@conduction.nl>
2021-08-12 08:59:32 +0200

    First tests

---
+++ api/src/Entity/Review.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\ReviewRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ReviewRepository::class)
+ */
+#[ApiResource]
+class Review
+{
+    /**
+     + The id of this review.
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * The rating of this review (between 0 and 5).
+     *
+     * @ORM\Column
+     */
+    private int $rating = 0;
+
+    /**
+     * The body of the review.
+     *
+     * @ORM\Column
+     */
+    private string $body = "";
+
+    /**
+     * The author of the review.
+     *
+     * @ORM\Column
+     */
+    private string $author = "";
+
+    /**
+     * The date of publication of this review.
+     *
+     * @ORM\Column(nullable=true)
+     */
+    private ?\DateTimeInterface $publicationDate = null;
+
+    /**
+     * The book this review is about.
+     * 
+     * @ORM\ManyToOne(targetEntity=Book::class, inversedBy="reviews")
+     */
+    private $book;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRating(): ?int
+    {
+        return $this->rating;
+    }
+
+    public function setRating(int $rating): self
+    {
+        $this->rating = $rating;
+
+        return $this;
+    }
+
+    public function getBody(): ?string
+    {
+        return $this->body;
+    }
+
+    public function setBody(string $body): self
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getPublicationDate(): ?\DateTimeInterface
+    {
+        return $this->publicationDate;
+    }
+
+    public function setPublicationDate(?\DateTimeInterface $publicationDate): self
+    {
+        $this->publicationDate = $publicationDate;
+
+        return $this;
+    }
+
+    public function getBook(): ?Book
+    {
+        return $this->book;
+    }
+
+    public function setBook(?Book $book): self
+    {
+        $this->book = $book;
+
+        return $this;
+    }
+}
