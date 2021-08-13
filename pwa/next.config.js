5efc5ae0596913f3f99005c45e150d08c45aa73d
Ruben van der Linde <ruben@conduction.nl>
2021-08-13 16:25:10 +0200

    Switched to material ui as a base plate

--- pwa/next.config.js
+++ pwa/next.config.js
@@ -1,8 +1,5 @@
-const withTM = require('next-transpile-modules')(['@utrecht/web-component-library-react']);
 
 module.exports = {
-  reactStrictMode: true,
-  ...withTM(),
   serverRuntimeConfig: {
     NEXT_PUBLIC_ENTRYPOINT: process.env.NEXT_PUBLIC_ENTRYPOINT || "http://localhost",
   },
