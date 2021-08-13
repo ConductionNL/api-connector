1e419aa64353b991204f1b28356e078dbe769085
Ruben van der Linde <ruben@conduction.nl>
2021-08-13 14:27:02 +0200

    NL design fix

--- pwa/next.config.js
+++ pwa/next.config.js
@@ -1,5 +1,10 @@
+const withTM = require('next-transpile-modules')(['@utrecht/web-component-library-react']);
+
 module.exports = {
+  reactStrictMode: true,
+  ...withTM(),
   serverRuntimeConfig: {
     NEXT_PUBLIC_ENTRYPOINT: process.env.NEXT_PUBLIC_ENTRYPOINT || "http://localhost",
   },
 };
+
