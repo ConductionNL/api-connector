220c946ac8e4aa7ba159b3df1e2a9d23cd1d96bf
Ruben van der Linde <ruben@conduction.nl>
2021-08-16 09:51:18 +0200

    Added materiaal design test

--- pwa/pages/index.tsx
+++ pwa/pages/index.tsx
@@ -1,11 +1,42 @@
 import Head from "next/head";
 import Link from "next/link";
 
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 const Welcome = () => (
   <>
     <Head>
       <title>Welcome to API Platform!</title>
     </Head>
+
+    <Container>
+      <Logo/>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start"  color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" >
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+
+          <a href="/docs">
+            <Button color="inherit">API</Button>
+          </a>
+        </Toolbar>
+      </AppBar>
+    </Container>
+
 
     <div className="welcome">
       <header className="welcome__top">
