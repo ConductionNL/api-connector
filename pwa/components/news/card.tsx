fad58fc40d7d2c2bb693488c31125a64707cba8f
Sarai Misidjan <sarai@conduction.nl>
2021-08-30 16:05:12 +0200

    updated news card

--- pwa/components/news/card.tsx
+++ pwa/components/news/card.tsx
@@ -1,24 +1,56 @@
 import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 import Grid from "@material-ui/core/Grid";
-import StandardCard from "../../components/common/card";
 
-export default function NewsCards({news = null}) {
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 300,
+  },
+  media: {
+    height: 200,
+  },
+});
+
+export default function NewsCard({news = null}) {
+  const classes = useStyles();
 
   return (
     <Grid container spacing={2}>
       {
         news !== null &&
         news.map((post) => (
-        <Grid item xs={4}>
-          <StandardCard
-            smallUpperTitle={post.title}
-            title={post.name}
-            secondaryTitle="12-04-2020"
-            description={post.description}
-            link={"/news/"+post.id}
-          />
-        </Grid>
-      ))}
+          <Grid item xs={4}>
+            <Card className={classes.root}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image="/static/images/cards/contemplative-reptile.jpg"
+                  title="Contemplative Reptile"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {post.name}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    {post.description}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Learn More
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
     </Grid>
   );
 }
