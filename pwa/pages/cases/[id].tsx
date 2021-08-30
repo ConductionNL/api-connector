7576630de85cea89887c26b9653628e1555693dd
Barry Brands <barrybrands02@hotmail.com>
2021-08-30 11:46:39 +0200

    Messages improvements

--- pwa/pages/cases/[id].tsx
+++ pwa/pages/cases/[id].tsx
@@ -1,20 +1,12 @@
 import {useRouter} from 'next/router'
-import Button from "@material-ui/core/Button";
 import React from "react";
-import Link from 'next/link'
 
-import Header from "../../components/common/header";
-import Container from "@material-ui/core/Container";
-import Footer from "../../components/common/footer";
-import Typography from '@material-ui/core/Typography';
 import Layout from "../../components/common/layout";
 import Grid from "@material-ui/core/Grid";
 import Hidden from "@material-ui/core/Hidden";
 import ActionMenu from "../../components/common/actionmenu";
 import PageHeader from "../../components/common/pageheader";
 import Box from "@material-ui/core/Box";
-import StandardCard from "../../components/common/card";
-import ArrangementsTable from "../../components/arrangements/table";
 
 const Product = () => {
   const title = "Trouwen | 95128942"
