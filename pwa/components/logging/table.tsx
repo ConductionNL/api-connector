3cb33c29911375644ec25f4992618d7c71e8fa24
Ruben van der Linde <ruben@conduction.nl>
2021-09-06 10:38:02 +0200

    Small fixes on routing and datatable

--- pwa/components/logging/table.tsx
+++ pwa/components/logging/table.tsx
@@ -35,40 +35,30 @@ export default function LoggingTable() {
     },
     {
       field: 'handelingsNaam',
-      headerName: 'handeling',
+      headerName: 'Handeling',
       width: 150,
       editable: true,
     },
     {
       field: 'verwerkingsnaam',
-      headerName: 'verwerking',
+      headerName: 'Verwerking',
       width: 300,
       editable: true,
     }
   ];
 
-  const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-  ];
-
   return (
    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={logging}
-        columns={columns}
-        pageSize={100}
-        rowsPerPageOptions={[100]}
-        checkboxSelection
-        disableSelectionOnClick
-      />
+
+     <DataGrid
+       rows={logging}
+       columns={columns}
+       pageSize={100}
+       rowsPerPageOptions={[100]}
+       checkboxSelection
+       disableSelectionOnClick
+     />
+
     </div>
   );
 }
