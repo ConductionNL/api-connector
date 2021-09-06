e9eb3fe65ddbc2fc59919fb1be351561dd99e40a
Ruben van der Linde <ruben@conduction.nl>
2021-09-06 08:57:20 +0200

    Exemple for datatable on verwerkingen

--- pwa/components/logging/table.tsx
+++ pwa/components/logging/table.tsx
@@ -6,48 +6,69 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import { DataGrid } from '@mui/x-data-grid';
+import {useGet} from "restful-react";
 
 function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
 }
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
 
 export default function LoggingTable() {
 
+  var { data: logging } = useGet({
+    path: "gateways/logging/verwerkings_acties"
+  });
+
+  /* lets catch hydra */
+  if (logging != null && logging["hydra:member"] !== undefined) {
+    logging = logging["hydra:member"];
+  }
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+      field: 'actieNaam',
+      headerName: 'Actie',
+      width: 300,
+      editable: true,
+    },
+    {
+      field: 'handelingsNaam',
+      headerName: 'handeling',
+      width: 150,
+      editable: true,
+    },
+    {
+      field: 'verwerkingsnaam',
+      headerName: 'verwerking',
+      width: 300,
+      editable: true,
+    }
+  ];
+
+  const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  ];
+
   return (
-    <TableContainer component={Paper}>
-      <Table aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+   <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={logging}
+        columns={columns}
+        pageSize={100}
+        rowsPerPageOptions={[100]}
+        checkboxSelection
+        disableSelectionOnClick
+      />
+    </div>
   );
 }
